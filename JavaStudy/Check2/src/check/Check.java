2a8c582fcc49706688daff27192d12d6ce7f5455
sahiko0319 <112632459+sahiko0319@users.noreply.github.com>
2022-09-07 21:30:34 +0900

    課題提出(添削後再修正)
    定数クラスの値を参照するように修正

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,5 +1,7 @@
 package check;
 
+import constants.Constants;
+
 public class Check {
 
 	private static String firstName = "武蔵";
@@ -7,10 +9,10 @@ public class Check {
 	
 	public static void main(String[] args) { 
 		printName(firstName,lastName);
-		Pet pet = new Pet("java吉","hoge");
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 		pet.introduce();
 		
-		RobotPet robotPet = new RobotPet("R2D2","ルーク");
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
 		robotPet.introduce();
 		
 	}
